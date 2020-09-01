6d78853c3388e3a576997a2c0a8e0262283dd9b6
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2020-09-01 22:55:40 +0200

    Added media queries to About section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -6,11 +6,20 @@ import { AboutContentParagraph, singleAdditionalSkill } from "../utils/types";
 
 const AboutWrapper = styled.section`
   width: 80%;
+  max-width: 1500px;
   min-height: 300px;
   margin: 100px auto 65px auto;
   background-color: ${({ theme }) => theme.lightGray};
   border-radius: 85px;
   padding: 40px;
+  @media screen and (min-width: 1600px) {
+    min-height: 500px;
+    padding: 80px;
+  }
+  @media screen and (max-width: 560px) {
+    width: 100%;
+    border-radius: 50px;
+  }
 `;
 
 const PortraitImg = styled.img`
@@ -19,6 +28,18 @@ const PortraitImg = styled.img`
   height: 200px;
   float: left;
   margin-right: 50px;
+  @media screen and (min-width: 1600px) {
+    width: 250px;
+    height: 250px;
+    margin-right: 80px;
+  }
+
+  @media screen and (max-width: 768px) {
+    shape-outside: none;
+    display: flex;
+    float: initial;
+    margin: 0 auto 30px auto;
+  }
 `;
 
 const AboutContent = styled.p`
@@ -31,6 +52,11 @@ const AboutContent = styled.p`
     font-weight: bold;
     font-size: ${({ theme }) => theme.fontSizes.M};
   }
+
+  @media screen and (min-width: 1600px) {
+    font-size: ${({ theme }) => theme.fontSizes.L};
+    line-height: 35px;
+  }
 `;
 const AdditionalSkillsSection = styled.section`
   width: 100%;
@@ -38,22 +64,36 @@ const AdditionalSkillsSection = styled.section`
 `;
 const AdditionalSkillWrapper = styled.article`
   width: 80%;
-  height: 70px;
+  min-width: 340px;
+  max-width: 1500px;
+  min-height: 70px;
   background-color: ${({ theme }) => theme.lightGray};
   display: flex;
   border-radius: 45px;
   margin: 5px auto 30px auto;
   align-items: center;
+  padding-right: 15px;
+
+  @media screen and (min-width: 1600px) {
+    font-size: ${({ theme }) => theme.fontSizes.L};
+    min-height: 90px;
+  }
 `;
 
 const PointerImage = styled.img`
   width: 35px;
   margin: 0 20px;
+  @media screen and (min-width: 1600px) {
+    width: 40px;
+  }
 `;
 
 const AdditionalSkillContent = styled.p`
   font-family: "Montserrat";
   font-size: ${({ theme }) => theme.fontSizes.S};
+  @media screen and (min-width: 1600px) {
+    font-size: ${({ theme }) => theme.fontSizes.L};
+  }
 `;
 
 const AboutSection = () => {
@@ -127,3 +167,26 @@ const renderAdditionalSkills = (
     );
   });
 };
+
+/*
+ @media screen and (min-width: 1600px) {
+    margin: 0 12vw 80px 0;
+    width: 350px;
+  }
+  @media screen and (max-width: 1280px) {
+    margin: 0 6vw 35px 0;
+    width: 280px;
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 200px;
+    margin: 0 8vw 35px 0;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 150px;
+    margin: 0 6vw 30px 0;
+  }
+  @media screen and (max-width: 560px) {
+    display: none;
+  } */
