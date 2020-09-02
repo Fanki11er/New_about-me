8a212c9cb3d8f07a0311c87442e5f3d5407e916c
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2020-09-02 22:36:51 +0200

    Added media queries to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -14,17 +14,32 @@ const StyledHeaderWrapper = styled(HeaderWrapper)`
 `;
 
 const NotFoundImage = styled.img`
-  display: flex;
-  justify-self: flex-end;
-  align-self: flex-end;
+  position: absolute;
+  bottom: 0;
+  right: 0;
   width: 320px;
-  margin-left: 30%;
-  margin-right: 80px;
-  margin-bottom: 75px;
+  margin: 0 10vw 100px 0;
 
+  @media screen and (min-width: 1600px) {
+    margin: 0 12vw 80px 0;
+    width: 350px;
+  }
   @media screen and (max-width: 1280px) {
-    margin-left: 25%;
-    margin-bottom: 80px;
+    margin: 0 6vw 60px 0;
+    width: 300px;
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 220px;
+    margin: 0 6vw 60px 0;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 180px;
+    margin: 0 4vw 60px 0;
+  }
+  @media screen and (max-width: 560px) {
+    display: none;
   }
 `;
 
