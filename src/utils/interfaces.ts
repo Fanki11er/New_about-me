7e5f658d11cf1f0c1cc4473bdd96017a434e26ff
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2020-09-03 22:41:32 +0200

    Moved menuElement interface to interfaces file

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,3 +1,13 @@
+import { ReactChild } from "react";
+
 export interface BackgroundImageUrl {
   imageUrl: string;
+}
+
+export interface MenuElementProps {
+  linkToPage: string;
+  label: string;
+  children: ReactChild;
+  newWindow?: boolean;
+  externalLink?: boolean;
 }
