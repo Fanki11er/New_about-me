4bec36e4217ca86ff33b6402ddcf9d7203a500ca
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2020-09-03 23:37:15 +0200

    Added color changing to Mobile menu

--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
+import NavigationList from "./NavigationList";
+import { PageColorsContext } from "../Providers/PageColorsProvider";
+import { Color } from "../utils/interfaces";
 
 const NavWrapper = styled.nav`
   position: fixed;
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: center;
   top: 0;
   right: 0;
   width: 100vw;
   height: 100vh;
-  clip-path: circle(150% at 90% -10%);
-  background-color: ${({ theme }) => theme.darkerBlue};
-
+  clip-path: circle(1000px at 90% -10%);
+  background-color: ${(props: Color) => props.color};
+  min-width: 360px;
   z-index: 7;
 `;
 
 const MobileNavigation = () => {
+  const { currentColor } = useContext(PageColorsContext);
   return (
-    <NavWrapper>
-      <p></p>
+    <NavWrapper color={currentColor}>
+      <NavigationList />
     </NavWrapper>
   );
 };
