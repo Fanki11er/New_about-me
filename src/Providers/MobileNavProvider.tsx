24cb3d4286e8dc47502a442ed5751a4af75914b8
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2020-09-05 15:19:59 +0200

    Modified MobileNavProvider
    Add better types to button and navigation
    Export object with references

--- src/Providers/MobileNavProvider.tsx
+++ src/Providers/MobileNavProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, RefObject } from "react";
 interface Clickable {
   onClick: Function;
 }
@@ -6,10 +6,16 @@ interface Hidable {
   show: Function;
 }
 
+type RefToButton = RefObject<any & Clickable> | null;
+type RefToMobileNav = RefObject<any & Hidable> | null;
 export const MobileNavContext = React.createContext({
   isMobileNavOpen: false,
   toggleNavModal: () => {},
-  getReferences: (button: Clickable, navigation: Hidable) => {},
+  getReferences: (button: RefToButton, navigation: RefToMobileNav) => {},
+  references: {
+    MobileNavButton: null,
+    MobileNavigation: null,
+  },
 });
 interface Props {
   children: React.ReactChild;
@@ -19,24 +25,26 @@ const MobileNavProvider = (props: Props) => {
   const { children } = props;
 
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
-  const [refToButton, setRefToButton] = useState<Clickable | null>(null);
-  const [refToNavigation, setRefToNavigation] = useState<Hidable | null>(null);
+  const [refToButton, setRefToButton] = useState<RefToButton>(null);
+  const [refToNavigation, setRefToNavigation] = useState<RefToMobileNav>(null);
 
   const toggleNavModal = () => {
     setIsMobileNavOpen(!isMobileNavOpen);
   };
 
-  const getReferences = (button: Clickable, navigation: Hidable) => {
+  const getReferences = (button: RefToButton, navigation: RefToMobileNav) => {
     if (!refToButton && button) setRefToButton(button);
     if (!refToNavigation && navigation) setRefToNavigation(navigation);
   };
-
-  useEffect(() => {});
 
   const context = {
     isMobileNavOpen,
     toggleNavModal,
     getReferences,
+    references: {
+      MobileNavButton: refToButton?.current,
+      MobileNavigation: refToNavigation?.current,
+    },
   };
   return (
     <MobileNavContext.Provider value={context}>
