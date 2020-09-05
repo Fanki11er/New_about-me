24680aacaf00dd8c073769499b0c97103e9f9ffc
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2020-09-05 22:27:47 +0200

    Added show and hide animation to MobileNavigation

--- src/Providers/MobileNavProvider.tsx
+++ src/Providers/MobileNavProvider.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { RefToButton, RefToMobileNav } from "../utils/types";
+import gsap from "gsap";
 
 export const MobileNavContext = React.createContext({
   isMobileNavOpen: false,
@@ -29,6 +30,45 @@ const MobileNavProvider = (props: Props) => {
     if (!refToButton && button) setRefToButton(button);
     if (!refToNavigation && navigation) setRefToNavigation(navigation);
   };
+
+  useEffect(() => {
+    if (refToButton && refToNavigation) {
+      const buttonRef = refToButton?.current;
+      const navigation = refToNavigation?.current;
+      const links = navigation.querySelectorAll("li");
+      const tl = gsap.timeline();
+      tl.set(links!, { opacity: 0 });
+
+      tl.fromTo(
+        navigation!,
+        { clipPath: "circle(100px at 90% -10%)" },
+        {
+          clipPath: "circle(1000px at 90% -10%)",
+          duration: "1",
+        }
+      )
+        .to(
+          links,
+          {
+            duration: 0.4,
+            opacity: 1,
+            stagger: {
+              grid: [6, 1],
+              from: "start",
+              amount: 0.5,
+            },
+          },
+          "=-0.7"
+        )
+        .paused(true);
+
+      if (isMobileNavOpen) {
+        tl.play();
+      } else {
+        tl.timeScale(2).reverse(2);
+      }
+    }
+  }, [isMobileNavOpen]);
 
   const context = {
     isMobileNavOpen,
--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect } from "react";
 import styled from "styled-components";
 import NavigationList from "./NavigationList";
 import { PageColorsContext } from "../Providers/PageColorsProvider";
+import { MobileNavContext } from "../Providers/MobileNavProvider";
 import { Color } from "../utils/interfaces";
 
 const NavWrapper = styled.nav`
@@ -12,24 +13,22 @@ const NavWrapper = styled.nav`
   right: 0;
   width: 100vw;
   height: 100vh;
-  clip-path: circle(
-    ${(props: NavProps) => (!props.isOpen ? "100px" : "1000px")} at 90% -10%
-  );
-  background-color: ${(props: Color & NavProps) => props.color};
+  clip-path: circle(100px at 90% -10%);
+  background-color: ${(props: Color) => props.color};
   min-width: 360px;
   z-index: 7;
 `;
 
 const MobileNavigation = () => {
   const { currentColor } = useContext(PageColorsContext);
-  const { isMobileNavOpen, getReferences } = useContext(MobileNavContext);
+  const { getReferences } = useContext(MobileNavContext);
   const nav = useRef<HTMLUnknownElement>(null);
   useEffect(() => {
     getReferences(null, nav);
   }, []);
 
   return (
-    <NavWrapper color={currentColor} isOpen={isMobileNavOpen} ref={nav}>
+    <NavWrapper color={currentColor} ref={nav}>
       <NavigationList />
     </NavWrapper>
   );
