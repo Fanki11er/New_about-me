4e0da885807ab1a5ba5da36cc08d04b805a50090
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2020-09-05 14:35:22 +0200

    Added getReferences function
    I need this to get references to button and mobile navigation elements

--- src/Providers/MobileNavProvider.tsx
+++ src/Providers/MobileNavProvider.tsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
+interface Clickable {
+  onClick: Function;
+}
+interface Hidable {
+  show: Function;
+}
 
 export const MobileNavContext = React.createContext({
   isMobileNavOpen: false,
   toggleNavModal: () => {},
+  getReferences: (button: Clickable, navigation: Hidable) => {},
 });
 interface Props {
   children: React.ReactChild;
@@ -12,9 +19,16 @@ const MobileNavProvider = (props: Props) => {
   const { children } = props;
 
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const [refToButton, setRefToButton] = useState<Clickable | null>(null);
+  const [refToNavigation, setRefToNavigation] = useState<Hidable | null>(null);
 
   const toggleNavModal = () => {
     setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+  const getReferences = (button: Clickable, navigation: Hidable) => {
+    if (!refToButton && button) setRefToButton(button);
+    if (!refToNavigation && navigation) setRefToNavigation(navigation);
   };
 
   useEffect(() => {});
@@ -22,6 +36,7 @@ const MobileNavProvider = (props: Props) => {
   const context = {
     isMobileNavOpen,
     toggleNavModal,
+    getReferences,
   };
   return (
     <MobileNavContext.Provider value={context}>
