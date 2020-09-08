d25ac16dbe9dfdd9853e74dc56a79dfb79e31382
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2020-09-08 23:01:05 +0200

    Deleted delay from MobileNav animation

--- src/Providers/MobileNavProvider.tsx
+++ src/Providers/MobileNavProvider.tsx
@@ -77,7 +77,7 @@ const MobileNavProvider = (props: Props) => {
           setIsInProgress(false);
         });
       } else {
-        tl.timeScale(1.7).reverse(10).delay(0.1);
+        tl.timeScale(1.7).reverse(10);
       }
     }
   }, [isMobileNavOpen]);
