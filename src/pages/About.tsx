35b37c41c97ae5c13dbe2be4b66ed6c88f16ad0d
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2020-09-19 20:01:54 +0200

    Added Cv downloading

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import SEO from "../components/seo";
+import { useStaticQuery, graphql } from "gatsby";
 import TopWrapper from "../components/TopWrapper";
 import useBackgroundImage from "../Hooks/useBackgroundImage";
 import HeaderWrapper from "../components/HeaderWrapper";
@@ -43,8 +44,88 @@ const PersonImage = styled.img`
     display: none;
   }
 `;
+
+const CvButton = styled.a`
+  position: absolute;
+  left: 25%;
+  bottom: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 55px;
+  background-color: ${({ theme }) => theme.darkerBlue};
+  border-radius: 20px;
+  color: ${({ theme }) => theme.turquoise};
+  font-size: ${({ theme }) => theme.fontSizes.M};
+
+  transition: color 0.4s;
+  margin-bottom: 25px;
+  z-index: 7;
+
+  span {
+    margin-left: 5px;
+    color: ${({ theme }) => theme.orange};
+  }
+
+  &:hover {
+    &:visited {
+      color: ${({ theme }) => theme.veryLightBlue};
+    }
+  }
+  &:visited {
+    color: ${({ theme }) => theme.turquoise};
+  }
+
+  @media screen and (min-width: 1600px) {
+    margin-bottom: 80px;
+  }
+
+  @media screen and (max-width: 1280px) {
+    margin-bottom: 20px;
+  }
+
+  @media screen and (max-width: 960px) {
+    font-size: ${({ theme }) => theme.fontSizes.S};
+    margin-bottom: 20px;
+    width: 160px;
+    height: 50px;
+    border-radius: 15px;
+    margin-left: -10px;
+  }
+  @media screen and (max-width: 768px) {
+    width: 120px;
+    height: 40px;
+    font-size: ${({ theme }) => theme.fontSizes.XS};
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+
+  @media screen and (max-width: 568px) {
+    margin-left: -15px;
+  }
+`;
+
 const AboutPage = () => {
   const { about_background, person } = useBackgroundImage();
+  const {
+    allDatoCmsCv: { nodes },
+  } = useStaticQuery(graphql`
+    {
+      allDatoCmsCv {
+        nodes {
+          cv {
+            url
+          }
+        }
+      }
+    }
+  `);
+
+  const {
+    cv: { url },
+  } = nodes[0];
+
   return (
     <>
       <SEO title="About" />
@@ -55,6 +136,9 @@ const AboutPage = () => {
               About<span>me</span>
             </StyledHeader>
           </HeaderWrapper>
+          <CvButton href={url} target="_blank">
+            Open <span>CV</span>
+          </CvButton>
           <PersonImage src={person} alt={"Standing person"} />
         </>
       </TopWrapper>
